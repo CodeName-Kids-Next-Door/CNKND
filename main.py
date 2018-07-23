28930c8ffa74851866abeee74f43061bd33d9911
AdeyemiTA <adeyemit.adeyemo@gmail.com>
2018-07-23 11:54:15 -0400

    Updated main.py
    updated main.py

--- main.py
+++ main.py
@@ -19,23 +19,40 @@ class MainPage(webapp2.RequestHandler):
 class LoginPage(webapp2.RequestHandler):
     def post(self):
         login_template = \
-                jinja_current_directory.get_template('templates/login.html')
-
-class CreateANewAccountPage(webapp2.RequestHandler):
-    def get(self):
-        NewAccount_template = \
-                jinja_current_directory.get_template('templates/newaccount.html')
-        self.response.write(welcome_template.render)
+              jinja_current_directory.get_template('templates/login.html')
+        self.response.write(login_template.render())
 
 class ProfilePage(webapp2.RequestHandler):
     def get(self):
         Profile_template = \
+                jinja_current_directory.get_template('templates/profile.html')
+        self.response.write(profile_template.render())
 
+class TournamentCreatorPage(webapp2.RequestHandler):
+    def get(self):
+        tournament_Creator_template = \
+                   jinja_current_directory.get_template('templates/tournament_Creator.html')
+        self.response.write(tournament_Creator_template.render())
+
+class TournmanetParticipatePage(webapp2.RequestHandler):
+    def get(self):
+        tournament_Participate_template = \
+            jinja_current_directory.get_template('templates/tournament_Participate.html')
+        self.response.write(tournament_Participate_template.render())
+
+class TournamentViewerPage(webapp2.RequestHandler):
+    def get(self):
+        tournament_Viewer_template = \
+            jinja_current_directory.get_template('templates/tournament_Viewer.html')
+        self.response.write(tournament_Viewer_template.render())
 
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
     ('/login', LoginPage),
-    ('/newaccount', CreateANewAccountPage)
+    ('/profile', ProfilePage)
+    ('/tournament_Creator', TournamentCreatorPage)
+    ('/tournament_Participate', TournmanetParticipatePage)
+    ('/tournament_Viewer', TournmanetViewerPage)
 ], debug=True)
